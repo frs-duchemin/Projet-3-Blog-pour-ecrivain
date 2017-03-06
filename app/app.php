f73278d4c0a849b4606d1093f5894994af8b5f10
frs <frs.duchemin@gmail.com>
2017-03-06 10:51:03 +0100

    ajout erreur 404

--- app/app.php
+++ app/app.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
+use Symfony\Component\HttpFoundation\Request;
 
 // Register global error and exception handlers
 ErrorHandler::register();
@@ -60,3 +61,18 @@ $app['dao.comment'] = function ($app) {
 
     return $commentDAO;
 };
+
+// Register error handler
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    switch ($code) {
+        case 403:
+            $message = 'Access denied.';
+            break;
+        case 404:
+            $message = 'The requested resource could not be found.';
+            break;
+        default:
+            $message = "Something went wrong.";
+    }
+    return $app['twig']->render('error.html.twig', array('message' => $message));
+});
